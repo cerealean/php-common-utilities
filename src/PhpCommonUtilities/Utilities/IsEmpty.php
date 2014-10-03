95a86c86f7fdadaac70826a61f0102e999c56f15
Michael Crawford <cerealean@me.com>
2014-10-03 15:04:33 -0500

    Added none, all, and value methods. Also added php docblock comments

--- src/PhpCommonUtilities/Utilities/IsEmpty.php
+++ src/PhpCommonUtilities/Utilities/IsEmpty.php
@@ -7,6 +7,8 @@ namespace PhpCommonUtilities\Utilities;
 class IsEmpty {
 
     /**
+     * Returns true if any of the given values are empty. Otherwise returns false.
+     *
      * @param mixed ... values
      * @return bool
      */
@@ -22,6 +24,45 @@ class IsEmpty {
         }
 
         return $are_any_empty;
+    }
+
+    /**
+     * Returns true if none of the given values are empty. Otherwise returns false.
+     *
+     * @param mixed... values
+     * @return bool
+     */
+    public static function none(){
+        return !call_user_func_array("any", func_get_args());
+    }
+
+    /**
+     * Returns true if all of the given values are empty. Otherwise returns false.
+     *
+     * @return bool
+     */
+    public static function all(){
+        $are_all_empty = true;
+        $values = func_get_args();
+
+        foreach($values as $value){
+            if(!empty($value)){
+                $are_all_empty = false;
+                break;
+            }
+        }
+
+        return $are_all_empty;
+    }
+
+    /**
+     * Returns true if the given value is empty. Otherwise returns false. Simple OOP wrapper for the PHP native empty function.
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    public static function value($value){
+        return empty($value);
     }
 
 }

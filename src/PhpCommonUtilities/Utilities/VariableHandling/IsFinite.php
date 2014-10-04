be6f7fbd0d2837a85a21499d7855904167475b7b
Michael Crawford <cerealean@me.com>
2014-10-03 21:44:10 -0500

    Added IsFinite, IsInfinite, and IsNotANumber

---
+++ src/PhpCommonUtilities/Utilities/VariableHandling/IsFinite.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace PhpCommonUtilities\Utilities\VariableHandling;
+
+
+class IsFinite implements VariableHandling {
+    /**
+     * Returns true if any of the given values are finite. Otherwise returns false.
+     *
+     * @param mixed ... values
+     * @return bool
+     */
+    public static function any(){
+        $are_any = false;
+        $values = func_get_args();
+
+        foreach($values as $value){
+            if(is_finite($value)){
+                $are_any = true;
+                break;
+            }
+        }
+
+        return $are_any;
+    }
+
+    /**
+     * Returns true if none of the given values are finite. Otherwise returns false.
+     *
+     * @param mixed... values
+     * @return bool
+     */
+    public static function none(){
+        return !call_user_func_array("any", func_get_args());
+    }
+
+    /**
+     * Returns true if all of the given values are finite. Otherwise returns false.
+     *
+     * @return bool
+     */
+    public static function all(){
+        $are_all = true;
+        $values = func_get_args();
+
+        foreach($values as $value){
+            if(!is_finite($value)){
+                $are_all = false;
+                break;
+            }
+        }
+
+        return $are_all;
+    }
+
+    /**
+     * Returns true if the given value is finite. Otherwise returns false. Simple OOP wrapper for the PHP native empty function.
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    public static function value($value){
+        return is_finite($value);
+    }
+}
+ 

163d715b7884395f6b1cc1bb2363ded8c2d1890f
Michael Crawford <cerealean@me.com>
2014-10-03 20:51:45 -0500

    Added IsResource, IsScalar, and IsString

---
+++ src/PhpCommonUtilities/Utilities/VariableHandling/IsResource.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace PhpCommonUtilities\Utilities\VariableHandling;
+
+
+class IsResource implements VariableHandling {
+    /**
+     * Returns true if any of the given values are resources. Otherwise returns false.
+     *
+     * @param mixed ... values
+     * @return bool
+     */
+    public static function any(){
+        $are_any = false;
+        $values = func_get_args();
+
+        foreach($values as $value){
+            if(is_resource($value)){
+                $are_any = true;
+                break;
+            }
+        }
+
+        return $are_any;
+    }
+
+    /**
+     * Returns true if none of the given values are resources. Otherwise returns false.
+     *
+     * @param mixed... values
+     * @return bool
+     */
+    public static function none(){
+        return !call_user_func_array("any", func_get_args());
+    }
+
+    /**
+     * Returns true if all of the given values are resources. Otherwise returns false.
+     *
+     * @return bool
+     */
+    public static function all(){
+        $are_all = true;
+        $values = func_get_args();
+
+        foreach($values as $value){
+            if(!is_resource($value)){
+                $are_all = false;
+                break;
+            }
+        }
+
+        return $are_all;
+    }
+
+    /**
+     * Returns true if the given value is a resource. Otherwise returns false. Simple OOP wrapper for the PHP native empty function.
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    public static function value($value){
+        return is_resource($value);
+    }
+}
+ 
